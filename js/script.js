0ed1d43b2e4379d7de1dd473dad020a25f3bd538
VenkatSaiKotha <venkatsaikotha8143@gmail.com>
2026-07-28 09:24:22 +0000

    html

---
+++ js/script.js
@@ -0,0 +1,10 @@
+alert("Welcom to NRIIT Learning Management Syatem")
+let student_name = "Venkat sai"
+console.log("student name is: " + student name)
+let student_age = 21
+let course_name = "Python Ful Stack"
+console.log("student age is:"=student age,"course name is:" +course name)
+let fee = 10000
+let discount = 10
+let discount fee = fee -(fee * discount/100)
+console.log("Discount fee is:" +discounted fee)
