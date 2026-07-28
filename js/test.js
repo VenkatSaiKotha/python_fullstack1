aa69d2751aa170fbf1a703632215f8095f182350
VenkatSaiKotha <venkatsaikotha8143@gmail.com>
2026-07-28 10:28:13 +0000

    html

--- js/test.js
+++ js/test.js
@@ -1,1 +1,47 @@
 console.log("Hello World")
+// // alert("Welcome to NRIIT Learning Management System");
+
+// let student_name = "Venkat Sai";
+// console.log("Student name is: " + student_name);
+
+// let student_age = 21;
+// let course_name = "Python Full Stack";
+
+// console.log("Student age is: " + student_age);
+// console.log("Course name is: " + course_name);
+
+// let fee = 10000;
+// let discount = 10;
+// let discount_fee = fee - (fee * discount / 100);
+
+// console.log("Discount fee is: " + discount_fee);
+
+// let age = 18;
+
+// if (age >= 18) {
+//     console.log("Eligible for admission");
+// } else {
+//     console.log("Not eligible for admission");
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Iteration number: " + i);
+// }
+
+// const pi = 3.14;
+// console.log("Value of pi is: " + pi);
+
+// let Student = {
+//     name: "Venkat Sai",
+//     age: 21,
+//     course: "Python Full Stack",
+//     fee: 10000
+// };
+
+// console.log("Student details:", Student);
+
+// function greetStudent(name) {
+//     console.log("Hello " + name + ", Welcome to NRIIT Learning Management System");
+// }
+
+// greetStudent(student_name);
